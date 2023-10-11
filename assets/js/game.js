a7a6c4e8f11011653f982cc459d7223bfd7757bc
Pedro Rodrigues <pedronuel36@gmail.com>
2023-10-10 22:49:37 -0300

    feat: nova sprite

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,7 +12,7 @@ const background = {
   x: 0,
   y: canvas.height - 204,
   draw() {
-    context.fillStyle = "#70c5ce";
+    context.fillStyle = "#2FE6FF";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     context.drawImage(
